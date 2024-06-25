0d9cd05ed620ebdb30ec2e68d4fa12936bbdaeb4
wangshu <wangshu@wangshudeMacBook-Air.local>
2024-06-25 14:57:44 +0900

    original

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -69,12 +69,12 @@ export type SectionId = (typeof SectionId)[keyof typeof SectionId];
  */
 export const heroData: Hero = {
   imageSrc: heroImage,
-  name: `I'm Shu Wang`,
+  name: `I'm Tim Baker.`,
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        I'm a  <strong className="text-stone-100">SNU Science Fellow</strong>, currently working
-        at <strong className="text-stone-100">Basic Institute in Seoul National University</strong> helping build a modern, mobile-first, domain
+        I'm a Victoria based <strong className="text-stone-100">Full Stack Software Engineer</strong>, currently working
+        at <strong className="text-stone-100">Instant Domains</strong> helping build a modern, mobile-first, domain
         registrar and site builder.
       </p>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
